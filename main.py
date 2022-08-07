8870f8f5a3d71b65c7d0789b2c00094dde8837ca
Aadrij Upadya <41775053+aadrijupadya@users.noreply.github.com>
2022-08-07 03:18:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,65 @@ from flask_uploads import UploadSet, IMAGES, configure_uploads
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
+
 import os
+import torch
+import numpy as np
+import os
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch.utils.data import DataLoader, Dataset
+from PIL import Image
+import numpy as np
+import torchvision.models as models
+import torch.nn as nn
+import torch.optim as optim
+import urllib.request
+import pickle
+
+
+def set_device():
+    if torch.cuda.is_available():
+        dev = "cuda:0"
+    else:
+        dev = "cpu"
+    return torch.device(dev)
+
+
+device = set_device()
+
+
+def pre_image(image_path, model):
+
+    img = Image.open(image_path)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    transform_norm = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Resize((224, 224)), transforms.Normalize(mean, std)])
+    # get normalized image
+    img_normalized = transform_norm(img).float()
+    img_normalized = img_normalized.unsqueeze_(0)
+    # input = Variable(image_tensor)
+    img_normalized = img_normalized.to(device)
+    # print(img_normalized.shape)
+    with torch.no_grad():
+        model.eval()
+        output = model(img_normalized)
+       # print(output)
+        index = output.data.cpu().numpy().argmax()
+        classes = {0: 'Definitely Healthy', 1: 'Definitely Unhealthy',
+                   2: 'Healthy', 3: 'Unhealthy'}
+        m = nn.Softmax(dim=1)
+        results = m(output)
+        results = sorted(results)
+        class_name = classes[index]
+        result_name = f'{class_name}, Confidence Value: {round(torch.max(results[0]).item() * 100, 2)} %'
+        print(result_name)
+        return result_name
+
+
+filename = 'resnet18_torch_trained_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
 
 app = Flask(__name__)
@@ -15,7 +73,7 @@ app.permanent_session_lifetime = timedelta(days=5)
 
 app.config["MYSQL_HOST"] = 'localhost'
 app.config["MYSQL_USER"] = "root"
-app.config['MYSQL_PASSWORD'] = "ffrn1234"
+app.config['MYSQL_PASSWORD'] = "Aadrij2005"
 app.config['MYSQL_DB'] = "learn_users"
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
@@ -41,14 +99,19 @@ class UploadForm(FlaskForm):
 def home():
     if "loggedin" in session:
         form = UploadForm()
+        result = pre_image(
+            '/Users/aadrijupadya/Downloads/cheetos.jpeg', loaded_model)
         if form.validate_on_submit():
             print("working")
             filename = photos.save(form.photo.data)
+            result = pre_image('uploads/'+filename, loaded_model)
+            print(result)
             file_url = url_for('get_file', filename=filename)
         else:
             file_url = None
             print("not working")
-        return render_template("loggedin.html", username=session["username"], form=form, file_url=file_url)
+        return render_template("loggedin.html", username=session["username"], form=form, file_url=file_url, result=result)
+
     else:
         return render_template("index.html")
     return render_template("index.html")
